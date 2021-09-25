d9eac6f2af3128353986667d57ed3d6362fa0077
Andrei Korikov <kpanic666@gmail.com>
2021-09-25 19:43:36 +0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Andrei Korikov on 25.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
