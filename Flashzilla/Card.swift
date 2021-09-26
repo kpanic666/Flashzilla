ab96ca8da2c171ef28a7ddcbda9591873f0208c4
Andrei Korikov <kpanic666@gmail.com>
2021-09-27 00:17:55 +0500

    Main screen and card view Add drag gesture handler and function to delete cards

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Andrei Korikov on 26.09.2021.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static var example: Card {
+        Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
